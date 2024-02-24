e4785a3662ef8848465c63b7a56f9b51744756ec
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-02-24 21:20:42 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+// Preload Effect
+const preload = document.querySelector("[data-preload]");
+window.addEventListener("load", function () {
+  preload.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+// Adding EventListners on multiple elements
+const addEventOnElement = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+// Navbar Toggling
+const navBar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglerNavbar = function () {
+  navBar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+addEventOnElement(navTogglers, "click", togglerNavbar);
+// Header drop
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector(".back-top-btn");
+let lastScrollPos = 0;
+const hideHeader = () => {
+  const isScrollButton = (lastScrollPos = window.scrollY);
+  if (isScrollButton) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollPos = window.scrollY;
+};
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+// Hero Slider
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSlidePos = () => {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+  updateSlidePos();
+};
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+  updateSlidePos();
+};
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+// Auto Slide
+let autoSlideInterval;
+const autoSlide = function () {
+  audioSlideInterval = setInterval(function () {
+    slideNext();
+  }, 7000);
+};
+addEventOnElement(
+  [heroSliderNextBtn, heroSliderPrevBtn],
+  "mouseover",
+  function () {
+    clearInterval(autoSlideInterval);
+  }
+);
+addEventOnElement(
+  [heroSliderNextBtn, heroSliderPrevBtn],
+  "mouseout",
+  autoSlide
+);
+
+window.addEventListener("load", autoSlide);
+
+//  Parallax effect
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+let x, y;
+window.addEventListener("click", function (event) {
+  x = (event.clientX / window.innerWidth) * 10 - 5;
+  y = (event.clientY / window.innerHeight) * 10 - 5;
+  x = x - (x + 2);
+  y = y - (y + 2);
+  for (let i = 0, len = parallaxItems.length; i < len; i++) {
+    x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+    y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+    parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+});
+// Back to top Button
